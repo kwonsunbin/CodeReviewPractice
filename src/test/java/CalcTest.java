dfdb6c28cdb5249b7d5c10fd61908faddb97a4a7
pyeonggang <pyeonggangkim@gmail.com>
2026-06-01 09:52:22 +0900

    [feature] getMinus, getDevide 메서드 테스트 코드 추가

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalcTest {
+    @Test
+    void getMinus() {
+        Calc c = new Calc();
+        assertEquals(10, c.getMinus(12, 2));
+    }
+
+    @Test
+    void getDevide() {
+        Calc c = new Calc();
+        assertEquals(2, c.getDevide(10, 5));
+    }
 }
