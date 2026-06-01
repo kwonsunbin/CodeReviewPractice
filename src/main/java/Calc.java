27527ee2ec9e08f977b373bd1acb9a57c96ceafa
jennkimm <smartshk98@gmail.com>
2026-06-01 09:51:09 +0900

    add getSumSum feature

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,2 +1,5 @@
 public class Calc {
+    public int getSumSum(int i, int i1, int i2) {
+        return i + i1 + i2;
+    }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalcTest {
+    private final Calc calc = new Calc();
+
+    @Test
+    void 세_양수의_합() {
+        assertThat(calc.getSumSum(1, 2, 3)).isEqualTo(6);
+    }
+
+    @Test
+    void 음수_포함_합() {
+        assertThat(calc.getSumSum(-1, -2, -3)).isEqualTo(-6);
+    }
+
+    @Test
+    void 모두_0이면_0() {
+        assertThat(calc.getSumSum(0, 0, 0)).isEqualTo(0);
+    }
+
+    @Test
+    void 양수_음수_혼합() {
+        assertThat(calc.getSumSum(10, -3, 5)).isEqualTo(12);
+    }
 }
